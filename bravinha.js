45ce54f99d5ab266c21a3e7ff5653860af7c3811
Daniel Santos <danielsantos.prog.sp@gmail.com>
2025-04-12 09:46:44 -0300

    pause project

--- bravinha.js
+++ bravinha.js
@@ -3,74 +3,65 @@
  * @autor Daniels Hogans - 2023
  * @email daniel.santos.ap@gmail.com
  */
-function ObserverBind(
-    idElementObserved, 
-    idElementStatus, 
-    textMessage=''){
-    const { fromEvent } = rxjs;
-    const input = document.getElementById(idElementObserved);
-    const statusElement = document.getElementById(idElementStatus);
-    const inputObservable = fromEvent(input, 'input');
-    inputObservable.subscribe(() => {
-        statusElement.textContent = textMessage + input.value ;
-        });
-}
-function lazySpa(){
+    /**
+     * funçãp que pega os dados de uma url e retorna um json, e passa para uma função de callback
+     * @param {string} url - URL da API 
+     * @param {function} callback - Função de callback que recebe os dados
+     * @returns {void}
+     */
+    function getDataCallback(url, callback){
 
-}
-const { Observable } = rxjs;
-const observable = new Observable((subscriber) => {
-subscriber.next('reduzir tempo');
-subscriber.next('ferramentas e framework');
-subscriber.next('escala');
-setTimeout(() => {
-    subscriber.next('carregamento dinâmico');
-    subscriber.complete();
-}, 3000);
-});
-
-console.log('Porquê criar um framework full?');
-observable.subscribe({
-next(x) {
-    console.log('precisamos: ' + x);
-},
-error(err) {
-    console.error('something wrong occurred: ' + err);
-},
-complete() {
-    console.log('Finalizado');
-},
-});
-console.log('Por isso estamos desenvolvendo o futuro');
-function alertdata(data){
-    alert(data[0].cep);
-}
-function getDataUrl(url, callback){
-    fetch(url).then(response => {
-        if (!response.ok) {
-        throw new Error('Erro na requisição');
-        }
-        return response.json();
-        }).then(data => {
-            callback(data);
-        }).catch(error => {
-            console.error(error);
-        });
-};
-    let url = 'https://jsonplaceholder.typicode.com/posts';
+        fetch(url).then(response => {
+            if (!response.ok) {
+            throw new Error('Erro na requisição');
+            }
+            return response.json();
+            }).then(data => {
+                callback(data);
+            }).catch(error => {
+                console.error(error);
+            });
+    };
     /** 
-     * 
-    */
+     * função para pegar os dados online em alguma api no formato json
+     * @param {string} url - URL da API
+     * @returns {json} - Retorna um json com os dados da API
+     */
     async function getData(url)
     {
         return await fetch(url)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                throw new Error('Erro na requisição');
             }
             return response.json();
-        })
-    } 
+        }).catch(error => {
+            console.error(error);
+        });
+    }
+    /**
+     * Função que pega os templates HTML e substitui os placeholders pelos dados
+     * @param {html} templateName - Arquivo que será carregado
+     * @param {json} data - Dados que serão substituidos no template
+     * @returns {html} - Retorna o html com os dados substituidos
+     */
+    async function loadTemplate(templateName, data) {
+        try {
+          // carregar o template HTML
+          const response = await fetch(`${templateName}.html`);
+          // converte o html em text
+          const templateHtml = await response.text();
+          const htmlFinal = templateHtml.replace(
+            /{{(\w+)}}/g, 
+            (match, placeholder) => data[placeholder] || ''
+          );
+          
+          return htmlFinal;
+        } catch (erro) {
+          console.error('Erro ao carregar o template:', erro);
+          return '';
+        }
+      }
     async function exibirPosts() {
             const posts = await getData(url);
             const container = document.getElementById('container');
@@ -88,29 +79,17 @@ function getDataUrl(url, callback){
             });
         }
         // Executar quando o documento carregar
-        // document.addEventListener('DOMContentLoaded', exibirPosts);
-        async function carregarTemplate(templateName, dados) {
-          try {
-            const response = await fetch(`${templateName}.html`); 
-            const templateHtml = await response.text();
-
-            const htmlFinal = templateHtml.replace(
-              /{{(\w+)}}/g, 
-              (match, placeholder) => dados[placeholder] || ''
-            );
-            
-            return htmlFinal;
-          } catch (erro) {
-            console.error('Erro ao carregar o template:', erro);
-            return '';
-          }
-        }
+        
         const dados = {
           nome: "Maria Oliveira",
           email: "maria@exemplo.com",
           idade: 28
         };
-        carregarTemplate('template', dados)
+        loadTemplate('template', dados)
         .then(html => {
           document.getElementById('container2').innerHTML = html;
         });
+        /**
+         * Não colocar nada depois desta ultima instrução
+         */
+        document.addEventListener('DOMContentLoaded', exibirPosts);
